5f27c499e16b4238923e753ef9fc2fcb18876354
Yagnesh Modh <yagneshmodh@Yagneshs-MacBook-Pro.local>
2020-11-19 00:48:26 +0530

    commit code for project setup

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('how are you')
+// REPL
+
+// R -> Read
+// E -> Eval
+// P -> Print
+// L -> Loop
+
+var http = require('http');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    })
+    res.end('Hello World')
+}).listen(8081);
+
+console.log('Server running on port 8081');
+
+
+
