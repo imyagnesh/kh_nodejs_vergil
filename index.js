8278e3779ee65b07ffb7252865cd6170781af02e
Yagnesh Modh <yagneshmodh@Yagneshs-MacBook-Pro.local>
2020-12-02 23:33:35 +0530

    completed with mongodb

--- index.js
+++ index.js
@@ -4,54 +4,68 @@ var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var hbs = require("express-handlebars");
+const Handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 
-// initialise express
+var routes = require("./routes/index");
+
 var app = express();
 
-// set view engine
+// view engine setup
+app.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defaultLayout: "layout",
+    layoutsDir: __dirname + "/views/layouts/",
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+  })
+);
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "hbs");
 
-// set all middlewares
-app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-// added static content
-app.use(express.static(path.resolve(__dirname, "public")));
+app.use("/", routes);
 
-// set the route
-var router = require('./routes/index')
-app.use("/", router);
-
-// eerror handling for pag not found
+// catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error("Page Not found");
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
-// error handling for developemt
+// error handlers
+
+// development error handler
+// will print stacktrace
 if (app.get("env") === "development") {
-  app.use(function (err, req, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render("error", {
       message: err.message,
-      err: err,
+      error: err,
     });
   });
 }
 
-// something wrong in my code
-app.use(function (err, req, next) {
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error", {
     message: err.message,
-    err: {},
+    error: {},
   });
 });
 
-app.listen(8081, function (err) {
-  console.log("your app is running on port 8081");
+app.listen(8080, function () {
+  console.log("listening");
 });
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,160 @@
 var express = require("express");
 var router = express.Router();
+const { body, validationResult } = require("express-validator");
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb://imyagnesh:abcd1234@ds117829.mlab.com:17829/kh_mongodb_vergil?retryWrites=false",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+var Schema = mongoose.Schema;
 
-router.get('/', function(req, res, next) {
-    res.render('index', {
-        title: "Home page",
-        condition: false
-    });
-})
+const courses = new Schema(
+  {
+    courseName: { type: String, required: true },
+    Duration: { type: Number, max: 100, min: 12 },
+    author: { type: String },
+    pre: String,
+  },
+  { collation: "courses" }
+);
+
+var Courses = mongoose.model('courses', courses);
+
+// const db = require("monk")(
+//   "mongodb://imyagnesh:abcd1234@ds117829.mlab.com:17829/kh_mongodb_vergil?retryWrites=false"
+// );
+
+router.get("/", function (req, res, next) {
+  res.render("index");
+});
+
+router.get("/get-data", function (req, res, next) {
+  Courses.find().then((data) => {
+    console.log("🚀 ~ file: index.js ~ line 33 ~ Courses.find ~ data", data)
+    res.render("index", { items: data });
+  });
+});
+
+router.post("/insert", function (req, res, next) {
+  var item = {
+    courseName: req.body.courseName,
+    Duration: req.body.Duration,
+    author: req.body.author,
+    pre: req.body.pre,
+  };
+  var data = new Courses(item);
+  data.save();
+  res.redirect('/')
+});
+
+router.post("/update", function (req, res, next) {
+  var id = req.body.id;
+
+  Courses.findById(id, function(err, doc) {
+      if(err) {
+        console.log('no record found')
+      }
+      doc.courseName = req.body.courseName;
+      doc.Duration = req.body.Duration;
+      doc.author = req.body.author;
+      doc.pre = req.body.pre;
+      doc.save();
+  })
+
+  res.redirect('/')
+});
+
+router.post("/delete", function (req, res, next) {
+  var id = req.body.id;
+  Courses.findByIdAndRemove(id).exec();
+  res.redirect('/')
+});
+
+// var express = require("express");
+// var router = express.Router();
+// const { body, validationResult } = require("express-validator");
+// var mongo = require("mongodb").MongoClient;
+// var objectId = require('mongodb').ObjectID;
+// const assert = require("assert");
+
+// var url =
+//   "mongodb://imyagnesh:abcd1234@ds117829.mlab.com:17829/kh_mongodb_vergil";
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index');
+// });
+
+// router.get('/get-data', function(req, res, next) {
+//   var resultArray = [];
+//   mongo.connect(url, function(err, client) {
+//     assert.strictEqual(null, err);
+//     const db = client.db("kh_mongodb_vergil");
+//     const cursor = db.collection("courses").find();
+//     cursor.forEach(function(doc, err) {
+//       assert.strictEqual(null, err);
+//       resultArray.push(doc);
+//     }, function() {
+//       client.close();
+//       console.log("🚀 ~ file: index.js ~ line 27 ~ cursor.forEach ~ resultArray", JSON.stringify(resultArray))
+//       res.render('index', {items: resultArray});
+//     });
+//   });
+// });
+
+// router.post('/insert', function(req, res, next) {
+//   var item = {
+//     courseName: req.body.courseName,
+//     Duration: req.body.Duration,
+//     author: req.body.author,
+//     pre: req.body.pre
+//   };
+
+//   mongo.connect(url, function(err, client) {
+//     assert.strictEqual(null, err);
+//     const db = client.db("kh_mongodb_vergil");
+//     db.collection('courses').insertOne(item, function(err, result) {
+//       assert.strictEqual(null, err);
+//       db.close();
+//     });
+//   });
+
+//   res.redirect('/');
+// });
+
+// router.post('/update', function(req, res, next) {
+//   var item = {
+//     courseName: req.body.courseName,
+//     Duration: req.body.Duration,
+//     author: req.body.author,
+//     pre: req.body.pre
+//   };
+//   console.log("🚀 ~ file: index.js ~ line 60 ~ router.post ~ item", item)
+//   var id = req.body.id;
+
+//   mongo.connect(url, function(err, client) {
+//     assert.strictEqual(null, err);
+//     const db = client.db("kh_mongodb_vergil");
+//     db.collection('courses').updateOne({"courseName": req.body.courseName}, {$set: item}, function(err, result) {
+//       assert.strictEqual(null, err);
+//       console.log('Item updated');
+//       client.close();
+//     });
+//   });
+// });
+
+// router.post('/delete', function(req, res, next) {
+//   var id = req.body.id;
+
+//   mongo.connect(url, function(err, client) {
+//     assert.equal(null, err);
+//     const db = client.db("kh_mongodb_vergil");
+//     db.collection('courses').deleteOne({"_id": objectId(id)}, function(err, result) {
+//       assert.equal(null, err);
+//       console.log('Item deleted');
+//       client.close();
+//     });
+//   });
+// });
 
 module.exports = router;
