0eaf1a2241be8f3482e5d38d93efce4ca79e053f
Yagnesh Modh <yagneshmodh@Yagneshs-MacBook-Pro.local>
2020-12-08 23:24:07 +0530

    change code

--- src/models/index.js
+++ src/models/index.js
@@ -1,19 +1,17 @@
 const Sequalize = require("sequelize");
 const dbConfig = require("../config/index");
 
-// let sequalize;
+let sequalize;
 
-// if (dbConfig.DATABASE_URL) {
-//   sequalize = new Sequalize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-//     host: dbConfig.HOST,
-//     dialect: dbConfig.dialect,
-//     pool: dbConfig.pool,
-//   });
-// } else {
-  
-// }
-
-const sequalize = new Sequalize('postgres://jbnyhasarjnzlm:5bdf05f9bda38ec4ebec7b321719607ba0d4afbcb246420e6b7b5e3f4ea14e2d@ec2-54-235-158-17.compute-1.amazonaws.com:5432/d8cvnmd2agc6io');
+if (dbConfig.DATABASE_URL) {
+  sequalize = new Sequalize(dbConfig.DATABASE_URL);
+} else {
+  sequalize = new Sequalize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    pool: dbConfig.pool,
+  });
+}
 
 const db = {};
 
