dc9d20ddc871eb3a0cf1d69bb824f1ba2ad923de
Yagnesh Modh <yagneshmodh@Yagneshs-MacBook-Pro.local>
2020-12-08 23:04:35 +0530

    commit code

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  DATABASE_URL: process.env.DATABASE_URL || 'postgres://jbnyhasarjnzlm:5bdf05f9bda38ec4ebec7b321719607ba0d4afbcb246420e6b7b5e3f4ea14e2d@ec2-54-235-158-17.compute-1.amazonaws.com:5432/d8cvnmd2agc6io',
+  DATABASE_URL: process.env.DATABASE_URL,
   HOST: process.env.HOST || "127.0.0.1",
   USER: process.env.USER || "yagneshmodh",
   PASSWORD: process.env.PASSWORD || "",
