963c1ed22d938c79ab78186da67a21f7427c8876
Yagnesh Modh <yagneshmodh@Yagneshs-MacBook-Pro.local>
2020-12-08 22:59:02 +0530

    change code for database url

--- src/models/index.js
+++ src/models/index.js
@@ -1,30 +1,36 @@
-const Sequalize = require('sequelize');
-const dbConfig = require('../config/index')
+const Sequalize = require("sequelize");
+const dbConfig = require("../config/index");
 
-const sequalize = new Sequalize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+let sequalize;
+
+if (dbConfig.DATABASE_URL) {
+  sequalize = new Sequalize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
-    pool: dbConfig.pool
-})
+    pool: dbConfig.pool,
+  });
+} else {
+  sequalize = new Sequalize(dbConfig.DATABASE_URL);
+}
 
 const db = {};
 
 // db.Sequalize = Sequalize;
 db.sequalize = sequalize;
-db.tutorial = require('./tutorial.model.js')(sequalize);
-db.user = require('./user.model.js')(sequalize);
-db.role = require('./role.model.js')(sequalize);
+db.tutorial = require("./tutorial.model.js")(sequalize);
+db.user = require("./user.model.js")(sequalize);
+db.role = require("./role.model.js")(sequalize);
 
 db.role.belongsToMany(db.user, {
-    through: 'user_roles',
-    foreignKey: 'roleId',
-    otherKey: 'userId'
+  through: "user_roles",
+  foreignKey: "roleId",
+  otherKey: "userId",
 });
 
 db.user.belongsToMany(db.role, {
-    through: 'user_roles',
-    foreignKey: 'userId',
-    otherKey: 'roleId'
+  through: "user_roles",
+  foreignKey: "userId",
+  otherKey: "roleId",
 });
 
 db.ROLES = ["user", "admin", "moderator"];
